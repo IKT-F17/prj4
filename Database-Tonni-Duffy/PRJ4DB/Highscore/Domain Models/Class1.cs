53c89a14b73635da724a6e2345ac63e8653ccee4
DuffyBaerbar <au564095>
2018-10-25 15:38:49 +0200

    oprettet DB samt et template vi kan arbejde med

---
+++ Database-Tonni-Duffy/PRJ4DB/Highscore/Domain Models/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Highscore
+{
+    class Class1
+    {
+    }
+}
---
+++ Database-Tonni-Duffy/PRJ4DB/MapDB/dbo/Tables/DB2.sql
@@ -0,0 +1,7 @@
+﻿--
+-- Create Table    : 'DB2'   
+-- DB2ID           :  
+--
+CREATE TABLE DB2 (
+    DB2ID          INT IDENTITY(1,1) NOT NULL,
+CONSTRAINT pk_DB2 PRIMARY KEY CLUSTERED (DB2ID))
