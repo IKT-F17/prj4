c174a10dcc98b5862016f1aaea38364ed00da7eb
Tonni Lutze <tonni@witzell.dk>
2018-10-25 16:06:33 +0200

    new push ... missed some files.

---
+++ Database-Tonni-Duffy/PRJ4DB/Prj4DB/dbo/ICollection_PHS.sql
@@ -0,0 +1,7 @@
+﻿--
+-- Create Table    : 'ICollection_PHS'   
+-- ICollectionID   :  
+--
+CREATE TABLE ICollection_PHS (
+    ICollectionID  INT IDENTITY(1,1) NOT NULL,
+CONSTRAINT pk_ICollection_PHS PRIMARY KEY CLUSTERED (ICollectionID))
