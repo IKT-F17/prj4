d0a4a041a98b9c96fcaac5003cdf2b74807f6438
Mette <mettechrisjacobsen@hotmail.com>
2018-11-27 10:40:33 +0100

    Renamed DefensivUnittest to OFfensiveunittest

---
+++ TowerDefence/TowerDefence/UnitTestProject/Offensiveunits_UnitTests/UnitTest_MyLittlePony.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using 
+
+namespace UnitTestProject.Offensiveunits_UnitTests
+{
+    [TestClass]
+    public class UnitTest_MyLittlePony
+    {
+        // MyLittlePony uut_;
+           [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
