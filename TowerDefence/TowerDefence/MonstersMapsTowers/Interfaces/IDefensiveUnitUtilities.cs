e058fbf994d6318db7c1b8ab8ca2eee2f65efbd7
Mette <mettechrisjacobsen@hotmail.com>
2018-11-28 12:14:29 +0100

    Made DefensiveUnitUtillities class and interface, updated other DefensiveUnit classes.

---
+++ TowerDefence/TowerDefence/MonstersMapsTowers/Interfaces/IDefensiveUnitUtilities.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using MonstersMapsTowers.Class;
+
+
+namespace MonstersMapsTowers.Interfaces
+{
+    public interface IDefensiveUnitUtilities
+    {
+        IDefensiveUnit SpawnDefensivUnit(IDefensiveUnit type, IMaps map, IPlayer player);
+        void upgradUnit(ref IDefensiveUnit unit, IPlayer player);
+        void downgradeUnit(IDefensiveUnit unit, IPlayer player);
+    }
+}
