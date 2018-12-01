bdd67a77279ec51ae4a1ae4760919ef3210643b5
DuffyBaerbar <au564095>
2018-12-02 00:32:59 +0100

    Added functional GUI for login

---
+++ LoginGUI/TDGUI/TDGUI/View/NavigationService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Threading.Tasks;
+using TDGUI.ViewModel;
+using TDGUI.View;
+
+namespace TDGUI.View
+{
+
+    class NavigationService : INavigationService
+    {
+        Window currentWindow;
+        public void Show(BaseViewModel vm)
+        {
+            switch (vm.GetType().Name)
+            {
+                case "RegisterUserViewModel":
+                    currentWindow = new RegisterUserView();
+                    currentWindow.ShowDialog();
+                    break;
+                case "MainMenuViewModel":
+                    currentWindow = new MainMenu();
+                    currentWindow.ShowDialog();
+                    break;
+                case "PersonalHighscoreViewModel":
+                    currentWindow = new PersonalHighscore();
+                    currentWindow.ShowDialog();
+                    break;
+
+            }
+        }
+
+        public void Close()
+        {
+            currentWindow.Close();
+        }
+    }
+}
---
+++ LoginGUI/TDGUI/TDGUI/ViewModel/MainMenuViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using TDGUI.Model;
+using TDGUI.View;
+
+namespace TDGUI.ViewModel
+{
+    class MainMenuViewModel : BaseViewModel
+    {
+        public MainMenuViewModel(Account acc, INavigationService n)
+        {
+            _nav = n;
+            _acc = acc;
+        }
+
+
+        private ICommand _CreateHighscoreView;
+
+        public ICommand CreateHighscoreView
+        {
+            get
+            {
+                return _CreateHighscoreView ?? (_CreateHighscoreView = new RelayCommand(CreateHighscoreViewer));
+            }
+        }
+
+        private void CreateHighscoreViewer()
+        {
+            PersonalHighscoreViewModel vm = new PersonalHighscoreViewModel(_acc, _nav);
+            _nav.Show(vm);
+        }
+    }
+}
