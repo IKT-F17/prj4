ae36717bb21df3330cfc1b7877e5218f3ed67336
Mette <mettechrisjacobsen@hotmail.com>
2018-12-03 07:12:45 +0100

    try to test upgrad bank

---
+++ TowerDefence/TowerDefence/MonstersMapsTowers/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonstersMapsTowers.Class;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Player player = new Player("ib");
+        player.bank = 100;
+        player.updateBank(-20);
+        Console.WriteLine(player.bank);
+    }
+}
