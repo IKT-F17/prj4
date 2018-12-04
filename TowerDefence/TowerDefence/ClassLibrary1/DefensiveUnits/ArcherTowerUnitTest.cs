5bb1faecc06c800b57dcd3b386c675edb94ad63e
Mette <mettechrisjacobsen@hotmail.com>
2018-12-04 11:08:11 +0100

    added [TestFixture] to unit tests, and Integrationstest

--- TowerDefence/TowerDefence/ClassLibrary1/DefensiveUnits/ArcherTowerUnitTest.cs
+++ TowerDefence/TowerDefence/ClassLibrary1/DefensiveUnits/ArcherTowerUnitTest.cs
@@ -34,8 +34,6 @@ namespace ClassLibrary1.DefensiveUnits
             Assert.That(_uut.defensiveLevel, Is.EqualTo(1));
             Assert.That(_uut.unitCost, Is.EqualTo(20));
             Assert.That(_uut.unitValue, Is.EqualTo(20));
-
         }
-
     }
 }
