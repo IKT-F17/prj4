79c92e83586102097e53ac90b4c525f248c36b0c
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-12-04 13:23:03 +0100

    Back to scratch, MapsUnitTest might have to be deleted.

--- TowerDefence/TowerDefence/ClassLibrary1/MapsUnitTest.cs
+++ TowerDefence/TowerDefence/ClassLibrary1/MapsUnitTest.cs
@@ -16,7 +16,7 @@ namespace TowerDefenceUnitTest
 {
     class MapsUnitTest
     {
-        
+        /*
         Maps _uut;
         private MapFileReader fakeMapFile;
         private Goblin fakeOffensiveUnits;
@@ -25,7 +25,7 @@ namespace TowerDefenceUnitTest
         [SetUp]
         public void Setup()
         {
-            //_uut = new Maps("Map 1");
+            _uut = new Maps("");
             fakeMapFile = Substitute.For<MapFileReader>("Map 1");
         }
 
@@ -33,16 +33,7 @@ namespace TowerDefenceUnitTest
         [Test]
         public void TestMapValues()
         {
-            //Hardcoded test, can't pull from Map01.txt yet
-            fakeMapFile.mapName = "map 1";
-            fakeMapFile.initialPlayerBank = 100;
-            fakeMapFile.timeDelaybetweenSpawns = 10;
 
-            Assert.That(fakeMapFile.mapName, Is.EqualTo("map 1"));
-            Assert.That(fakeMapFile.initialPlayerBank, Is.EqualTo(100));
-            Assert.That(fakeMapFile.timeDelaybetweenSpawns, Is.EqualTo(10));
-
-            /*
             fakeMapFile.LoadMapFile("map 1");
             var mapName = fakeMapFile.mapName;
             var mapImageFilePath = fakeMapFile.mapImageFilepath;
@@ -53,9 +44,9 @@ namespace TowerDefenceUnitTest
             Assert.That(fakeMapFile.mapImageFilepath, Is.EqualTo(mapImageFilePath));
             Assert.That(fakeMapFile.initialPlayerBank, Is.EqualTo(initialPlayerBank));
             Assert.That(fakeMapFile.timeDelaybetweenSpawns, Is.EqualTo(timeDelaybetweenSpawns));
-            */
+            
 
-        }
+        } */
 
     }
 
