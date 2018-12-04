ad271c01a408026d998596620707c5b080b8c055
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-12-04 11:38:33 +0100

    Added Unit test for Maps, have included some code in it, just wanted to commit before pull

---
+++ TowerDefence/TowerDefence/ClassLibrary1/MapsUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MonstersMapsTowers.Interfaces;
+using MonstersMapsTowers.Class;
+using MonstersMapsTowers.Class.Pathing;
+using MonstersMapsTowers.Class.OffensiveUnits;
+using NSubstitute;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace TowerDefenceUnitTest
+{
+    class MapsUnitTest
+    {
+        Maps _uut;
+        private MapFileReader fakeMapFile;
+        private Player fakePlayer;
+        private Goblin fakeOffensiveUnits;
+
+        
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new Maps("");
+            fakeMapFile = Substitute.For<MapFileReader>();
+
+            fakePlayer = Substitute.For<Player>();
+            
+        }
+
+        [Test]
+        public void TestMapBeingLoaded()
+        {
+            fakeMapFile.LoadMapFile("Map01");
+            var mapName = fakeMapFile.mapName;
+            var mapImageFilePath = fakeMapFile.mapImageFilepath;
+            var initialPlayerBank = fakeMapFile.initialPlayerBank;
+            var timeDelaybetweenSpawns = fakeMapFile.timeDelaybetweenSpawns;
+
+            Assert.That(fakeMapFile.mapName, Is.EqualTo(mapName));
+            Assert.That(fakeMapFile.mapImageFilepath, Is.EqualTo(mapImageFilePath));
+            Assert.That(fakeMapFile.initialPlayerBank, Is.EqualTo(initialPlayerBank));
+            Assert.That(fakeMapFile.timeDelaybetweenSpawns, Is.EqualTo(timeDelaybetweenSpawns));
+
+        }
+        /*
+        public Maps(string mapName) //constructor
+        {
+            
+
+            var mapfile = new MapFileReader();
+            mapfile.LoadMapFile(mapName);
+            mapName = mapfile.mapName;
+            mapImageFilePath = mapfile.mapImageFilepath;
+
+            initialPlayerBank = mapfile.initialPlayerBank;
+
+            numberOfWaves = mapfile.numberOfWaves;
+            numberOfOffensiveUnits = mapfile.numberOfOffensiveUnits;
+            offensiveUnitType = mapfile.offensiveUnitType;
+            timeDelaybetweenSpawns = mapfile.timeDelaybetweenSpawns;
+
+            rawPath = mapfile.rawPath;
+        }
+        */
+    }
+
+}
