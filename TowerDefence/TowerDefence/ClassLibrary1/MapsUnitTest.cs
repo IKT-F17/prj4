fb16baf37a289a56c78b5f33585357299525c351
MikkelBleeg <mikkelbleeg@hotmail.com>
2018-12-04 12:36:09 +0100

    Test should be working, but missing text file to pull from for it to work

--- TowerDefence/TowerDefence/ClassLibrary1/MapsUnitTest.cs
+++ TowerDefence/TowerDefence/ClassLibrary1/MapsUnitTest.cs
@@ -15,6 +15,7 @@ namespace TowerDefenceUnitTest
 {
     class MapsUnitTest
     {
+        /*
         Maps _uut;
         private MapFileReader fakeMapFile;
         private Player fakePlayer;
@@ -24,17 +25,16 @@ namespace TowerDefenceUnitTest
         [SetUp]
         public void Setup()
         {
-            _uut = new Maps("");
-            fakeMapFile = Substitute.For<MapFileReader>();
-
-            fakePlayer = Substitute.For<Player>();
-            
+            _uut = new Maps("Map 1");
+            fakeMapFile = Substitute.For<MapFileReader>("Map 1");
+            fakePlayer = Substitute.For<Player>(); 
         }
 
         [Test]
         public void TestMapBeingLoaded()
         {
-            fakeMapFile.LoadMapFile("Map01");
+            
+            fakeMapFile.LoadMapFile("map 1");
             var mapName = fakeMapFile.mapName;
             var mapImageFilePath = fakeMapFile.mapImageFilepath;
             var initialPlayerBank = fakeMapFile.initialPlayerBank;
@@ -44,28 +44,10 @@ namespace TowerDefenceUnitTest
             Assert.That(fakeMapFile.mapImageFilepath, Is.EqualTo(mapImageFilePath));
             Assert.That(fakeMapFile.initialPlayerBank, Is.EqualTo(initialPlayerBank));
             Assert.That(fakeMapFile.timeDelaybetweenSpawns, Is.EqualTo(timeDelaybetweenSpawns));
+            */
 
         }
-        /*
-        public Maps(string mapName) //constructor
-        {
-            
 
-            var mapfile = new MapFileReader();
-            mapfile.LoadMapFile(mapName);
-            mapName = mapfile.mapName;
-            mapImageFilePath = mapfile.mapImageFilepath;
-
-            initialPlayerBank = mapfile.initialPlayerBank;
-
-            numberOfWaves = mapfile.numberOfWaves;
-            numberOfOffensiveUnits = mapfile.numberOfOffensiveUnits;
-            offensiveUnitType = mapfile.offensiveUnitType;
-            timeDelaybetweenSpawns = mapfile.timeDelaybetweenSpawns;
-
-            rawPath = mapfile.rawPath;
-        }
-        */
     }
 
 }
