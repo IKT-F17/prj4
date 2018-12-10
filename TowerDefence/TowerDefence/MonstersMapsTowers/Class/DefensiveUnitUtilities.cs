8d5d6f43d87f9cdab461e8fe0d99e1deae09288b
Mette <mettechrisjacobsen@hotmail.com>
2018-12-10 06:36:29 +0100

    code clean up

--- TowerDefence/TowerDefence/MonstersMapsTowers/Class/DefensiveUnitUtilities.cs
+++ TowerDefence/TowerDefence/MonstersMapsTowers/Class/DefensiveUnitUtilities.cs
@@ -8,7 +8,7 @@ using MonstersMapsTowers.Interfaces;
 
 namespace MonstersMapsTowers.Class
 {
-    public class DefensiveUnitUtilities:IDefensiveUnitUtilities
+    public class DefensiveUnitUtilities : IDefensiveUnitUtilities
     {
         private double consecutivePlacementCostFactor = 1.5; // this is the factor which changes the cost of placing a consecutive tower 
         private double upgradeCostFactor = 1.5; // this is the factor which changes the cost of upgrading a tower.  
@@ -77,17 +77,13 @@ namespace MonstersMapsTowers.Class
                 player.updateBank(-valuta);
 
                 unit.defensiveLevel = unit.defensiveLevel + 1;
-                //  if we need to be upgrade levels , we'd need the name to be something like: 
                 unit.nameDefensiveUnit = unit.nameDefensiveUnit + " Level " + unit.defensiveLevel;
-                unit.defensivePower = unit.defensivePower + 2; // think we should keep this to addition        
-                //tower.defenseType =
-                //    unit.defenseType; // only necessary if we actually change the tower type when upgrading
+                unit.defensivePower = unit.defensivePower + 2;
                 unit.defenseRange = unit.defenseRange + 1;
                 unit.unitValue += unit.upgradeCost;
                 unit.upgradeCost = unit.upgradeCost * upgradeCostFactor; //new upprice  
 
-                //  player.updateBank(-unit.upgradeCost); //subtrac the price from user bank
-                //add the tower to the map list of towers
+
             }
             else
             {
@@ -103,17 +99,12 @@ namespace MonstersMapsTowers.Class
                 player.updateBank(-valuta);
 
                 unit.defensiveLevel = unit.defensiveLevel + 1;
-                //  if we need to be upgrade levels , we'd need the name to be something like: 
                 unit.nameDefensiveUnit = unit.nameDefensiveUnit + " Level " + unit.defensiveLevel;
-                unit.defensivePower = unit.defensivePower + 2; // think we should keep this to addition        
-                //tower.defenseType =
-                //    unit.defenseType; // only necessary if we actually change the tower type when upgrading
+                unit.defensivePower = unit.defensivePower + 2;      
                 unit.defenseRange = unit.defenseRange + 1;
                 unit.unitValue += unit.upgradeCost;
                 unit.upgradeCost = unit.upgradeCost * upgradeCostFactor; //new upprice  
 
-                //  player.updateBank(-unit.upgradeCost); //subtrac the price from user bank
-                //add the tower to the map list of towers
             }
             else
             {
@@ -139,7 +130,7 @@ namespace MonstersMapsTowers.Class
                 unit.defensiveLevel = unit.defensiveLevel - 1;
                 unit.nameDefensiveUnit = "ArcherTower Level " + unit.defensiveLevel;
                 unit.defensivePower = unit.defensivePower - 2;
-                unit.defenseType = unit.defenseType;   // only necessary if we actually change the tower type when upgrading
+                unit.defenseType = unit.defenseType;   
                 unit.defenseRange = unit.defenseRange - 1;
                 unit.upgradeCost = unit.upgradeCost / upgradeCostFactor;
 
@@ -168,7 +159,7 @@ namespace MonstersMapsTowers.Class
                 unit.defensiveLevel = unit.defensiveLevel - 1;
                 unit.nameDefensiveUnit = "CannonTower Level " + unit.defensiveLevel;
                 unit.defensivePower = unit.defensivePower - 2;
-                unit.defenseType = unit.defenseType;   // only necessary if we actually change the tower type when upgrading
+                unit.defenseType = unit.defenseType;   
                 unit.defenseRange = unit.defenseRange - 1;
                 unit.upgradeCost = unit.upgradeCost / upgradeCostFactor;
 
